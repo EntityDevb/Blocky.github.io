058e80d65e928a22aa7024230369c7654797c3de
EntityDevb <71733647+EntityDevb@users.noreply.github.com>
2024-05-10 16:01:50 -0700

    Update commands.ts

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -7,18 +7,7 @@ const hostname = window.location.hostname;
 
 export const commands: Record<string, (args: string[]) => Promise<string> | string> = {
   help: () => 'Available commands: ' + Object.keys(commands).join(', '),
-  hostname: () => hostname,
-  whoami: () => 'guest',
-  date: () => new Date().toLocaleString(),
-  vi: () => `why use vi? try 'emacs'`,
-  vim: () => `why use vim? try 'emacs'`,
-  emacs: () => `why use emacs? try 'vim'`,
-  echo: (args: string[]) => args.join(' '),
-  sudo: (args: string[]) => {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-
-    return `Permission denied: unable to run the command '${args[0]}' as root.`;
-  },
+  date: () => '7th January 1976',
   theme: (args: string[]) => {
     const usage = `Usage: theme [args].
     [args]:
@@ -63,63 +52,42 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
       }
     }
   },
-  repo: () => {
-    window.open(packageJson.repository.url, '_blank');
-
-    return 'Opening repository...';
-  },
   clear: () => {
     history.set([]);
 
     return '';
   },
-  email: () => {
-    window.open(`mailto:${packageJson.author.email}`);
-
-    return `Opening mailto:${packageJson.author.email}...`;
-  },
-  donate: () => {
-    window.open(packageJson.funding.url, '_blank');
-
-    return 'Opening donation url...';
-  },
-  weather: async (args: string[]) => {
-    const city = args.join('+');
-
-    if (!city) {
-      return 'Usage: weather [city]. Example: weather Brussels';
-    }
-
-    const weather = await fetch(`https://wttr.in/${city}?ATm`);
-
-    return weather.text();
-  },
   exit: () => {
     return 'Please close the tab to exit.';
   },
-  curl: async (args: string[]) => {
+  geolocator: (args: string[]) => {
+    const usage = `Usage: theme [args].
+    [args]:
+      ip: locate the given IP
+    `;
     if (args.length === 0) {
-      return 'curl: no URL provided';
+      return usage;
     }
 
-    const url = args[0];
+    switch (args[0]) {
+      case '144.232.131.23': {
+        result += `Unable to Access, Please wait for a supervisor.`;
 
-    try {
-      const response = await fetch(url);
-      const data = await response.text();
+        return result;
+      }
 
-      return data;
-    } catch (error) {
-      return `curl: could not fetch URL ${url}. Details: ${error}`;
+      default: {
+        return usage;
+      }
     }
   },
   banner: () => `
-███╗   ███╗██╗  ██╗████████╗████████╗███████╗██████╗
-████╗ ████║██║  ██║╚══██╔══╝╚══██╔══╝╚════██║╚════██╗
-██╔████╔██║███████║   ██║      ██║       ██╔╝ █████╔╝
-██║╚██╔╝██║╚════██║   ██║      ██║      ██╔╝ ██╔═══╝
-██║ ╚═╝ ██║     ██║   ██║      ██║      ██║  ███████╗
-╚═╝     ╚═╝     ╚═╝   ╚═╝      ╚═╝      ╚═╝  ╚══════╝ v${packageJson.version}
+██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗██╗░░░██╗
+██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝╚██╗░██╔╝
+██████╦╝██║░░░░░██║░░██║██║░░╚═╝█████═╝░░╚████╔╝░
+██╔══██╗██║░░░░░██║░░██║██║░░██╗██╔═██╗░░░╚██╔╝░░
+██████╦╝███████╗╚█████╔╝╚█████╔╝██║░╚██╗░░░██║░░░
+╚═════╝░╚══════╝░╚════╝░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░ v${packageJson.version}
 
 Type 'help' to see list of available commands.
 `,
